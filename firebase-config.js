a4e86b29efcb2620c08d67b7a0d5a72ac63efc63
cheyoung1983-sudo <your-email@example.com>
2026-06-16 01:59:49 +0000

    feat: add firebase configuration and update package permissions

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAk9aTCTHoE6RAfKpt9WNDYxvUHtSUZ3aI",
+  authDomain: "displaycellpros-com.firebaseapp.com",
+  projectId: "displaycellpros-com",
+  storageBucket: "displaycellpros-com.firebasestorage.app",
+  messagingSenderId: "1046067704682",
+  appId: "1:1046067704682:web:ce3ca691c618904a5bab13",
+  measurementId: "G-E192YYWZKK"
+};
+
+export default firebaseConfig;
