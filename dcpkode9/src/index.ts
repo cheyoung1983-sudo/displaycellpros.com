3dc2d5d88e58f4bd463a0d7b367d0fc1e8b51fe1
cheyoung1983-sudo <cheyoung1983@gmail.com>
2026-07-01 03:01:21 -0700

    perf: add dns-prefetch and preconnect hints for Google and Firebase APIs

--- dcpkode9/src/index.ts
+++ dcpkode9/src/index.ts
@@ -10,7 +10,16 @@
 import {setGlobalOptions} from "firebase-functions";
 import {onRequest} from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
+// dcpkode9/src/index.ts
+import { setGlobalOptions } from "firebase-functions";
+import { onRequest } from "firebase-functions/v2/https";
+// Import your existing app logic from server.ts
+import { app } from "../../server";
 
+setGlobalOptions({ maxInstances: 10 });
+
+// Export the 'api' function that matches the rewrite rule
+export const api = onRequest({ maxInstances: 10 }, app);
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
